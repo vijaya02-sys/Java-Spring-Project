94ecefd902e4ac542d3b4560ac2e8471010d220d
vijaya <vilajaxmyai@gmail.com>
2024-07-02 16:11:29 +0530

    Core Java Concept

--- Core-Java/Hello.java
+++ Core-Java/Hello.java
@@ -1,2 +1,12 @@
+/**
+ * Hello
+ */
+class Hello 
+{
 
-System.out.println("Hello World");
+    public static void main(String args[]) 
+    {
+        System.out.println("Hello World");
+
+    }
+}

8db3c47195e696777c2f7cbf6c1681bcf7754e70
vijaya <vilajaxmyai@gmail.com>
2024-07-03 12:01:40 +0530

    Basic Java

--- Core-Java/Hello.java
+++ Core-Java/Hello.java
@@ -6,7 +6,231 @@ class Hello
 
     public static void main(String args[]) 
     {
+        
+        /*
+        
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
+        
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
+        
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
+        // LOOP  -- while, do{} while, for
+        
+        int i = 1;
+        
+        infinite LOOP
+        while(true) 
+        {
+            System.out.println("Hi");
+        }
+        
+        while(i<=4)
+        {
+            System.out.println("Hi" + i);
+            i++;
+        }
+        
+        while(i <= 4) 
+        {
+            System.out.println("Hi " + i);
+            int j = 1;
+            while (j <= 3) {
+                System.out.println("Hello");
+                j++;
+            }
+        }
+        
+        
+        i = 5;
+        do 
+        {
+            System.out.println("Hi " + i);
+        
+            i++;
+        }while(i<=4);
+        
+        
+        for(int j = 0; j < 5; j++) 
+        {
+            System.out.println("Hi " + j);
+        }
+            
+        for(int i = 1; i < = 5; i++) 
+        {
+            System.out.println("DAY" + i);
+        
+            for(int j=1; j<=9; j++)
+            {
+                System.out.println(" " + (j+8) + "-" + (j+9));
+            }
+            
+        }
+        
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+         // Conditional Statements
+        int c = 8;
+        
+        if(c > 10)
+            System.out.println("Hello");
+        else
+            System.out.println("Bye");
+            
+        
+        int x = 8;
+        int y = 7;
+        int z = 6;
+        
+        if (x > y && x > z) {
+            System.out.println("x is greatest: " + x);
+        }
+        else if (y > z) {
+            System.out.println("y is greatest: " + y);
+        }
+        else {
+            System.out.println("z is greatest: " + z);
+        }
+        
+        
+        // Ternary operator
+        if(n%2 == 0) 
+            result = 10;
+        
+        else 
+            result = 20;
+            
+        
+        
+        int n = 4;
+        int result = 0;
+        
+        result = n % 2 == 0 ? 10 : 20;
+        
+        System.out.println(result);
+        
+        // Switch
+        int n = 1;
+        
+        if(n==1)
+            System.out.println("Monday");
+        else if(n==2)
+            System.out.println("Tuesday");
+        else if(n==3)
+            System.out.println("Wednesday");
+        else if(n==4)
+            System.out.println("Thursday");
+        else if(n==5)
+            System.out.println("Friday");
+        else if(n==6)
+            System.out.println("Saturday");
+        else 
+            System.out.println("Sunday");
+        
+        
+        switch(n) {
+            case 1: 
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("enter valid number");
+        }
+        
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+         
+        // Logical Operators  -  &&, ||, !
+        int x = 7;
+        int y = 5;
+        int s = 5;
+        int t = 9;
+        
+        boolean result = x > y && s > t;      
+        
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        // Type promotion
+        byte a = 10;
+        byte b = 30;
+        
+        int result = a * b;
+        
+        // Type conversion and type casting 
+        
+        byte b = 127;
+        int a = b; // type conversion
+        
+        int c = 257;
+        b = (byte) c; // type casting
+        
+        float f = 5.6f;
+        int t = (int) f;  // type casting
+        
+        
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        
+        DATA TYPES
+        
+        // literals
+        
+        int num1 = 0b101;
+        int num2 = 0x7E;
+        
+        Integer
+        int num1 = 9;          -- 4 bytes
+        byte by = 127;         -- 1 byte = 8 bits = -128 to 127
+        short sh = 558;        -- 2 bytes
+        long l = 5854l;        -- 8 bytes
+        
+        Decimals
+        float marks = 6.5f;    -- 4 bytes
+        double d = 5.8;        -- 8 bytes
+        
+        Characters
+        char c = 'k';    // literals  -- 2 bytes
+        
+        Boolean
+        boolean b = true;            -- 1 byte
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        VARIABLES: 
+        int num1 = 3;
+        int num2 = 4;
+        
+        int result = num1 + num2;
+        
+        System.out.println(result);
+        
+        System.out.println(3 + 5);
+        System.out.println(6 + 7);
         System.out.println("Hello World");
+        System.out.println(num1);
+        */
 
     }
 }

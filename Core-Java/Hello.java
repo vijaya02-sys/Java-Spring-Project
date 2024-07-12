204babd3c3eabbdef904286342d186849f4367f0
vijaya <vilajaxmyai@gmail.com>
2024-07-12 15:47:20 +0530

    Core java final push

--- Core-Java/Hello.java
+++ Core-Java/Hello.java
@@ -1,36 +1,9 @@
-import Package_AccesModifier.*;
-
-// Access modifiers - 
-// public - if you want access anything from a package to the outside of package declare it as public
-// private - private variables can be used only in its class
-
-/*
- *                                   Public         Private     Protected    Default
- * same class                         Yes            Yes          Yes          Yes
- * same package subclass              Yes            No           Yes          Yes
- * same package non subclass          Yes            No           Yes          Yes
- * different package subclass         Yes            No           Yes          No
- * different package non-subclass     Yes            No           No           No
- * 
- */
-
-/*
- *
- * Note : 
- * i. try to declare your class public
- * ii. try to keep instance variable as private
- * ii. try to use public or protected for methods
- * 
- */
+//
 
 public class Hello {
 
-    public static void main(String args[]) throws ClassNotFoundException {
-        A obj = new A();
-        System.out.println(obj.marks);
+    public static void main(String args[]) {
 
-        B obj1 = new B();
-        System.out.println(obj1.marks);
+         
     }
 }
-
--- Core-Java/tools/VeryAdvClc.java
+++ Core-Java/tools/VeryAdvClc.java
@@ -1,6 +1,5 @@
+package tools;
 // Multilevel inheritance
-
-import tools.AdvCalc;
 
 public class VeryAdvClc extends AdvCalc{
     public double power(int n1, int n2)
